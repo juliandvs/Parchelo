8bf0f0b8a32becd12587857fa5acf62ab456051b
Julian Vera <juliandverasoto0714@gmail.com>
2023-11-11 16:49:10 -0500

    Se implemento la funcionalidad para poder navegar con el BottomBarNavegation

--- app/src/main/java/com/example/parchelo/Perfil.kt
+++ app/src/main/java/com/example/parchelo/Perfil.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- app/src/main/java/com/example/parchelo/menu.kt
+++ app/src/main/java/com/example/parchelo/menu.kt
@@ -1,10 +1,13 @@
 package com.example.parchelo
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.navigation.fragment.findNavController
 import com.example.parchelo.databinding.FragmentMenuBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,18 +16,74 @@ class menu : Fragment() {
 
     private lateinit var binding : FragmentMenuBinding
 
+    lateinit var navegacion : BottomNavigationView
+
+    private val OnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener {
+        item ->
+        when(item.itemId){
+            R.id.home ->{
+                childFragmentManager.commit{
+                    replace<Principal>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.guarda ->{
+                childFragmentManager.commit{
+                    replace<guardados>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.crea ->{
+                childFragmentManager.commit{
+                    replace<Crear>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.chatea ->{
+                childFragmentManager.commit{
+                    replace<chat>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.perfil_usuario->{
+                childFragmentManager.commit{
+                    replace<Perfil>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMenuBinding.inflate(inflater,container,false)
-
-
         val root = binding.root
-        val btn_nav = binding.bottomNavigationView
+        childFragmentManager.commit{
+            replace<Principal>(R.id.frameContainer)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+        navegacion = binding.bottomNavigationView
+        navegacion.setOnNavigationItemSelectedListener(OnNavMenu)
 
         return root
     }
+
+
+
 
 }
