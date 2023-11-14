e40ff91db85e76bd5c7682623ec6beabeb3661e1
Nicolis15 <nicolistrabajo@gmail.com>
2023-11-14 02:04:40 -0500

    Boton crear

--- app/src/main/java/com/example/parchelo/Crear.kt
+++ app/src/main/java/com/example/parchelo/Crear.kt
@@ -10,10 +10,12 @@ import androidx.navigation.fragment.findNavController
 import com.example.parchelo.databinding.FragmentCrearBinding
 import com.example.parchelo.databinding.FragmentGuardadosBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 private lateinit var binding: FragmentCrearBinding
-private lateinit var bar: BottomNavigationView
-class Crear : Fragment() {
+
+class Crear : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,35 +23,6 @@ class Crear : Fragment() {
     ): View? {
         binding = FragmentCrearBinding.inflate(inflater, container, false)
         val root = binding.root
-
-        bar = requireActivity().findViewById(R.id.bottomNavBar)
-        bar.isGone = false
-
-        bar.setOnItemSelectedListener {menuItem ->
-            when (menuItem.itemId){
-                R.id.home ->{
-                    findNavController().navigate(R.id.action_crear_to_principal)
-                    true
-                }
-                R.id.guarda ->{
-                    findNavController().navigate(R.id.action_crear_to_guardados)
-                    true
-                }
-                R.id.crea -> {
-                    true
-                }
-                R.id.chatea -> {
-                    findNavController().navigate(R.id.action_crear_to_chat)
-                    true
-                }
-                R.id.perfil_usuario -> {
-                    findNavController().navigate(R.id.action_crear_to_perfil)
-                    true
-                }
-                else -> false
-            }
-        }
-
         return root
     }
 }
